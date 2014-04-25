191b4f643045740a5fae1a1b8e25de33a65d1aff
Hemanta Sapkota <laex.pearl@gmail.com>
2014-04-25 22:24:34 +1000

    Error excercises

---
+++ error_excerciese.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "fmt"
+  "math"
+  )
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+  return fmt.Sprintf("Cannot square root negative number: %v", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+
+  if x < 0 {
+    return 0, ErrNegativeSqrt(x)
+  }
+
+  z := float64(1)
+  for i := 0; i < 10; i++ {
+    z = z - (math.Pow(z, 2) - x) / (2 * z)
+  }
+  return z, nil
+}
+
+func main() {
+  fmt.Println(Sqrt(2))
+  fmt.Println(Sqrt(-2))
+}
