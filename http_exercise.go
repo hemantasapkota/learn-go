775a55a6c221d0e3132d3b4c23a8bab78103a44d
Hemanta Sapkota <laex.pearl@gmail.com>
2014-04-26 11:20:02 +1000

    HTTP Excercise

---
+++ http_exercise.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "net/http"
+  "fmt"
+  )
+
+type String string
+
+type Struct struct {
+  Greeting string
+  Punct string
+  Who string
+}
+
+func (s String) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprint(w, s)
+}
+
+func (s Struct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "%s %s %s", s.Greeting, s.Punct, s.Who)
+}
+
+func main() {
+  http.Handle("/string", String("I'm a frayed knot."))
+  http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
+
+  fmt.Println("Listening on 4000")
+  http.ListenAndServe("localhost:4000", nil)
+}
