06e1b7f611e1bb536efcc9b70de1ff880b7a1925
Hemanta Sapkota <laex.pearl@gmail.com>
2014-04-29 12:40:44 +1000

    webcrawler excercise todo

--- webcrawler.go
+++ webcrawler.go
@@ -2,33 +2,74 @@ package main
 
 import (
   "fmt"
+  "errors"
+  "sync"
   )
 
 type Fetcher interface {
-  Fetch(url string, ch chan fakeResult)
+  Fetch(url string) (body string, urls []string, err error)
 }
 
-func Crawl(url string, depth int, fetcher Fetcher, ch chan fakeResult) {
+var fetched = struct {
+  m map[string]error
+  sync.Mutex
+}{m: make(map[string]error)}
+
+var loading = errors.New("url load in progress")
+
+func Crawl(url string, depth int, fetcher Fetcher) {
 
   if depth <= 0 {
+    fmt.Printf("<- Done with %v, depth 0\n")
     return
   }
 
-  go fetcher.Fetch(url, ch)
-
-  v := <-ch
-  fmt.Println(v.body)
-
-  for _,u := range v.urls {
-    Crawl(u, depth - 1, fetcher, ch)
+  fetched.Lock()
+  if _, ok := fetched.m[url]; ok {
+    fetched.Unlock()
+    fmt.Printf("<- Done with %v, already fetched.\n", url)
+    return
   }
 
+  fetched.m[url] = loading
+  fetched.Unlock()
+
+  body, urls, err := fetcher.Fetch(url)
+
+  fetched.Lock()
+  fetched.m[url] = err
+  fetched.Unlock()
+
+  if err != nil {
+    fmt.Printf("<- Error on %v: %v\n", url, err)
+  }
+
+  fmt.Printf("Found: %s %q", url, body)
+  done := make(chan bool)
+  for i, u := range urls {
+    fmt.Printf("-> Crawling child %v/%v of %v: %v.\n", i, len(urls), u)
+    go func(url string) {
+      Crawl(url, depth - 1, fetcher)
+    }(u)
+  }
+  for i := range urls {
+    fmt.Printf("<- [%v] %v/%v Waiting for child %v.\n", url, i, len(urls))
+    <-done
+  }
+  fmt.Printf("<- Done with %v/\n", url)
 }
 
 func main() {
-  ch := make(chan fakeResult)
-  Crawl("http://golang.org/", 4, fetcher, ch)
-  close(ch)
+  Crawl("http://golang.org/", 4, fetcher)
+
+  fmt.Println("Fetching stats\n--------------")
+  for url, err := range fetched.m {
+    if err != nil {
+      fmt.Printf("%v failed: %v\n", url, err)
+    } else {
+      fmt.Printf("%v was fetchd.\n", url)
+    }
+  }
 }
 
 // fakeFetcher is Fetcher that returns canned results.
@@ -39,14 +80,15 @@ type fakeResult struct {
     urls []string
 }
 
-func (f fakeFetcher) Fetch(url string, ch chan fakeResult) {
-  if res, ok := f[url]; ok {
-    ch <- *res
+func (f *fakeFetcher) Fetch(url string) (string, []string, error) {
+  if res, ok := (*f)[url]; ok {
+    return res.body, res.urls, nil
   }
+  return "", nil, fmt.Errorf("not found:%s", url)
 }
 
 // fetcher is a populated fakeFetcher.
-var fetcher = fakeFetcher{
+var fetcher = &fakeFetcher{
   "http://golang.org/": &fakeResult{
       "The Go Programming Language",
       []string{
